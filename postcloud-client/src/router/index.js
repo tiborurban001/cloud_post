7f0149c46ed58c4219fc0c6d75998ea5c3d62e85
tibor.urban001 <tibor.urban001@gmail.com>
2021-03-23 18:42:37 +0100

    Login route/component csatolása

--- postcloud-client/src/router/index.js
+++ postcloud-client/src/router/index.js
@@ -1,7 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+//Login component importálás
+import Login from '../views/Login.vue'
 
 const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
   {
     path: '/',
     name: 'Home',
---
+++ postcloud-client/src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    
+    <div>Login Page</div>
+
+</template>
+
+<script>
+export default {
+    name: 'login'
+    
+}
+</script>
+
+<style>
+
+</style>

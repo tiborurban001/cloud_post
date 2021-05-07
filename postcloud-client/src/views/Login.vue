c84a2a5f794f132fbbe1314649a927dc495dbc67
tibor.urban001 <tibor.urban001@gmail.com>
2021-05-07 09:22:50 +0200

    styling => .form-group - to global in App

--- postcloud-client/src/views/Login.vue
+++ postcloud-client/src/views/Login.vue
@@ -86,42 +86,6 @@ header {
     box-sizing: border-box;
     height: 100vh;
 
-    .form-group{
-        flex: 1;
-        display:flex;
-        justify-content: flex-start;
-        flex-flow: column;
-        padding: 28px;
-
-       input{
-           outline: none;
-            width: 100%;
-            height: 30px;
-            border: 1px solid rgb(33, 37, 41);
-            margin-bottom: 15px;
-
-           text-indent: 5px;
-            background-color:rgb(222, 226, 230) ;
-
-            &:focus{
-                border: 1.2px solid rgb(0, 129, 167);
-            }
-        }
-       button{
-           outline: none;
-           align-self: center;
-           width: 50%;
-           height: 35px;
-           background: rgb(0,119,182);
-           background: linear-gradient(0deg, rgba(0,119,182,1) 0%, rgba(0,180,216,1) 100%);
-           color: rgb(248, 249, 250);
-           appearance: none;
-           border:none;
-           border-radius: 25px;
-           font-weight: 700;
-           font-size: 19px;
-       }
-    }
     //Footer
     footer
         {

8080b2f38d603e9b6cfd63f12afe999fcf083aab
tibor.urban001 <tibor.urban001@gmail.com>
2021-05-13 13:57:30 +0200

    Postok kialakítása

--- postcloud-client/src/store/index.js
+++ postcloud-client/src/store/index.js
@@ -8,7 +8,33 @@ export default new Vuex.Store({
   state: {
     api_url: 'http://localhost:3000/',
     isAuthenticated: false,
-    
+    feed: [
+    {
+      id: 0,
+      user_id: 0,
+      display_name: 'Garrosh Hellscream',
+      desc: 'Lol, Dark Portal to Draenor go BRRRR...',
+      image: 'https://static.icy-veins.com/images/wow/og-image-siege-of-orgrimmar-garrosh-hellscream.jpg',
+      timestamp: 1620904791
+    },
+    {
+      id: 1,
+      user_id: 1,
+      display_name: 'Arthas',
+      desc: 'Where is my boney dragon?!',
+      image: 'https://i.imgflip.com/45cmpc.jpg',
+      timestamp: 1620904987
+    },
+    {
+      id: 2,
+      user_id: 2,
+      display_name: 'Khadgar',
+      desc: '*insert Hero of Azeroth here*',
+      image: 'https://i.redd.it/y3q32mv5mga21.jpg',
+      timestamp: 1620904973
+    }
+    ]
+
   },
   mutations: {
     isAuthenticated(state){
--- postcloud-client/src/views/Home.vue
+++ postcloud-client/src/views/Home.vue
@@ -1,12 +1,27 @@
 <template>
   <main class="feed">
-    Hello, World!
+
+    <article class="post" v-for="post in feed" :key="post.id">
+      <section class="post-user">{{post.diplay_name}}</section>
+      <section class="post-picture">
+        <img :src="post.image" :alt="post.desc" class="post-image">
+      </section>
+      <footer class="post-desc">
+        {{post.display_name + ': '+ post.desc}}
+      </footer>
+    </article>
+    
     <button @click='logout'>Logout</button>
   </main>
 </template>
 
 <script>
 export default {
+  computed: {
+    feed () {
+      return this.$store.state.feed;
+    }
+  },
   methods:{
     logout() {
       this.$store.commit('logout');

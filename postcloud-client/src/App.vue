bfb721def558fe77bf7346be8a3b410d608bddc3
tibor.urban001 <tibor.urban001@gmail.com>
2021-05-13 09:31:48 +0200

    Styling to main.scss

--- postcloud-client/src/App.vue
+++ postcloud-client/src/App.vue
@@ -6,17 +6,8 @@
 
 
 <style lang="scss">
-body{
- 
-  margin: 0;
-  padding: 0;
-  background-color:rgb(255,255,255);
-  font-family: 'Roboto', Helvetica, sans-serif;
-}
-#app{
-  width: 100wv;
-  min-height: 100hv;
-}
+@import './assets/scss/main.scss';
+
 
  .form-group{
         flex: 1;
